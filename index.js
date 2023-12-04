71814eb079334ed1ad7e5b6a9ef6276a79704278
Sen Feng <senfeng6@gmail.com>
2023-12-04 00:17:34 -0500

    Added database functionality for dream sharing feature

--- index.js
+++ index.js
@@ -13,8 +13,19 @@ app.use(cors({
   origin: 'http://localhost:3000'
 }));
 
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+const dbPath = 'dreams.db'; 
+
+let db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    createTable(db);
+    console.log('Connected to the dreams database.');
+});
 
 const openai = new OpenAI({
   apiKey: process.env.API_KEY,
@@ -22,23 +33,73 @@ const openai = new OpenAI({
 
 app.post('/interpret-dream', async (req, res) => {
   const userDream = req.body.dreamDescription;
-  // try {
-  //   const completion = await openai.chat.completions.create({
-  //     messages: [
-  //       { role: "system", content: "You are a helpful assistant knowledgeable in Zhou Gong's Dream Dictionary. You will respond in either English or Chinese based on the language of the dream described. You will limit your response within 50 words." },
-  //       { role: "user", content: `I had a dream where ${userDream}. Can you interpret this based on Zhou Gong's Dream Dictionary?"` },
-  //     ],
-  //     model: "gpt-3.5-turbo-1106",
-  //     max_tokens: 200,
-  //   });
-  //   res.send(`${completion.choices[0].message.content}`);
-  // } catch (error) {
-  //   console.error('Error:', error);
-  //   res.status(500).send('An error occurred');
-  // }
-  res.send(`Interpretation:`);
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "You are a helpful assistant knowledgeable in Zhou Gong's Dream Dictionary. You will respond in either English or Chinese based on the language of the dream described. You will limit your response within 50 words." },
+        { role: "user", content: `I had a dream where ${userDream}. Can you interpret this based on Zhou Gong's Dream Dictionary?"` },
+      ],
+      model: "gpt-3.5-turbo-1106",
+      max_tokens: 200,
+    });
+    res.send(`${completion.choices[0].message.content}`);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred');
+  }
+});
+
+app.get('/dreams', (req, res) => {
+  let sql = `SELECT * FROM dreams;`
+  db.all(sql, [], (err, rows) => {
+      res.send(rows);
+    });
+
+});
+
+app.post('/dream', (req, res) => {
+  let newName = req.body.name;
+  let newDescription = req.body.description;
+  let newLocation = req.body.location;
+
+  let sql = `
+    INSERT INTO dreams (name, description, location, timestamp)
+    VALUES (?, ?, ?, CURRENT_TIMESTAMP)
+  `;
+  
+  db.run(sql, [newName, newDescription, newLocation], function (err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error inserting dream into the database.');
+    } else {
+      res.status(200).send('Dream inserted successfully with ID: ' + this.lastID);
+    }
+  });
+  
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+function createTable(db) {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS dreams
+    (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      description TEXT NOT NULL,
+      timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+      location TEXT
+    );
+  `);
+}
+
+function cleanup() {
+  console.log('\nCleanup called');
+  db.close();
+  process.exit();
+}
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
--- public/dreamInterpreter.js
+++ public/dreamInterpreter.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // Hide the response div initially
         document.getElementById('dreamInterpretationResponse').style.display = 'none';
         showLoadingSpinner()
-        
+
         // AJAX call
         const dreamDescription = document.getElementById('dreamInput').value;
 
@@ -22,8 +22,50 @@ document.addEventListener('DOMContentLoaded', function() {
             const responseDiv = document.getElementById('dreamInterpretationResponse');
             responseDiv.innerHTML = data; 
             responseDiv.style.display = 'block'; 
+
+            document.getElementById('shareDream').style.display = 'block';
         })
         .catch(error => console.error('Error:', error));
+    });
+
+    document.getElementById('shareButton').addEventListener('click', function () {
+        // Get the text from the dreamInput textarea
+        const nameInputText = document.getElementById('nameInput').value;
+        const dreamInputText = document.getElementById('dreamInput').value;
+        const locationInputText = document.getElementById('locationInput').value;
+    
+        // Check if the input is empty
+        if (!dreamInputText.trim()) {
+            console.error('Dream description cannot be empty');
+            return;
+        }
+    
+        // Send an HTTP POST request to insert the text into the database
+        fetch('/dream', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: nameInputText,
+                description: dreamInputText,
+                location: locationInputText,
+            }),
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Dream shared successfully');
+                document.getElementById('dreamInput').value = '';
+                document.getElementById('nameInput').value = '';
+                document.getElementById('locationInput').value = '';
+            } else {
+                console.error('Failed to share dream');
+            }
+        })
+        .catch(error => {
+            // Handle network errors
+            console.error('Network error:', error);
+        });
     });
 });
 
